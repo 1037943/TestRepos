cb644680b4f10e58692f57047d9cde78601b8a7f
1037943 <1037943@01HW424708.India.TCS.com>
2016-10-26 16:52:18 +0530

    Commit 26/10/2016 16:52:18

---
+++ Deployment/InitW1/BankCustomer/scripts/database.sql
@@ -0,0 +1,20 @@
+--Start Writing Table ddls
+DROP TABLE CUSTOMERDETAILS;
+
+CREATE TABLE CUSTOMERDETAILS
+(
+	CUSTNUMBER NUMERIC(9,0)  NOT NULL  ,
+	CUSTNAME CHARACTER VARYING(256)  ,
+	CUSTAGE NUMERIC(9,0) ,
+	CustomerDetailsVer NUMERIC (4, 0)  NOT NULL ,
+	dmLastUpdateDate	TIMESTAMP WITH TIME ZONE	NOT NULL  ,
+	PRIMARY KEY ( 
+	CUSTNUMBER  )
+);
+--End Writing Table ddls
+--Start Writing Index ddls
+--End Writing Index ddls
+--Start Writing Keys ddls
+--End Writing Keys ddls
+--Start Writing Sequence ddls
+--End Writing Sequence ddls
