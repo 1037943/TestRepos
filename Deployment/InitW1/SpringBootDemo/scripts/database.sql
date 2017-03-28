32f1185006744666a14ed1970fb1e39dbe0493cb
1037943 <1037943@01HW424708.India.TCS.com>
2017-03-28 15:11:39 +0530

    Commit 28/03/2017 15:11:39

---
+++ Deployment/InitW1/SpringBootDemo/scripts/database.sql
@@ -0,0 +1,19 @@
+--Start Writing Table ddls
+DROP TABLE PERSISTCLASS;
+
+CREATE TABLE PERSISTCLASS
+(
+	ID NUMERIC(9,0)  NOT NULL  ,
+	NAME CHARACTER VARYING(256) ,
+	PersistClassVer NUMERIC (4, 0)  NOT NULL ,
+	dmLastUpdateDate	TIMESTAMP WITH TIME ZONE	NOT NULL  ,
+	PRIMARY KEY ( 
+	ID  )
+);
+--End Writing Table ddls
+--Start Writing Index ddls
+--End Writing Index ddls
+--Start Writing Keys ddls
+--End Writing Keys ddls
+--Start Writing Sequence ddls
+--End Writing Sequence ddls
