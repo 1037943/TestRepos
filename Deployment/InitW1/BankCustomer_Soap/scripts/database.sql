302eb98abd424983c002c51d33d24ec69f7b516b
962012 <962012@01HW394379.India.TCS.com>
2017-03-29 20:38:05 +0530

    Commit 29/03/2017 20:38:05

---
+++ Deployment/InitW1/BankCustomer_Soap/scripts/database.sql
@@ -0,0 +1,20 @@
+--Start Writing Table ddls
+DROP TABLE CUSTBANK;
+
+CREATE TABLE CUSTBANK
+(
+	ID NUMERIC(9,0)  NOT NULL  ,
+	NAME CHARACTER VARYING(256)  NOT NULL  ,
+	AGE NUMERIC(9,0) ,
+	CustBankVer NUMERIC (4, 0)  NOT NULL ,
+	dmLastUpdateDate	TIMESTAMP WITH TIME ZONE	NOT NULL  ,
+	PRIMARY KEY ( 
+	ID  )
+);
+--End Writing Table ddls
+--Start Writing Index ddls
+--End Writing Index ddls
+--Start Writing Keys ddls
+--End Writing Keys ddls
+--Start Writing Sequence ddls
+--End Writing Sequence ddls
